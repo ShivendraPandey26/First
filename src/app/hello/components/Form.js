978726d0dd2004e3e11243565ea8a9129838a429
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-07-28 23:51:35 +0530

    Create Form.js

---
+++ src/app/hello/components/Form.js
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <br />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      {formData.name === "" ? <h1> Fill the form </h1> : <h1>Hello {formData.name} sir!!</h1>}
+    </>
+  );
+};
+
+export default Form;
